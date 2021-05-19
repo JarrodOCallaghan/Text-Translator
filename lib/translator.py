73e186aa66ab7b9be6f7214e4bbfa611ace48992
Jarrod <jarrodocall@gmail.com>
2021-05-19 20:16:58 +1000

    Included requirements.txt

--- lib/translator.py
+++ lib/translator.py
@@ -2,27 +2,53 @@ class Translator():
     def __init__(self, dictionary):
         self.dictionary = dictionary
         self.wordlist = dictionary.wordlist
+
     def translate_single_word(self, language, word):
-        #Translate single word
+        # Translate single word
+        # Returning an array of words for when there are multiple
+        # interpretations, we can print all variations
+        found_words = []
+        if language == '-en':
+            for dict_word in self.wordlist:
+                if word.lower() == dict_word[1].lower():
+                    found_words.append(dict_word[0])
+                    print(dict_word[0])
+        elif language == '-ar':
+            for dict_word in self.wordlist:
+                if word.lower() == dict_word[0].lower():
+                    found_words.append(dict_word[1])
+        # if not found_words:
+        #    found_words.append("?")
+        return found_words
+
+# Need to split up to avoid duplication
+    def search_single_word(self, language, word):
+        # Translate single word
         found_words = ""
         if language == '-en':
             for dict_word in self.wordlist:
                 if word.lower() in dict_word[1].lower():
-                    found_words = found_words + ", " + dict_word[0]
+                    if found_words == "":
+                        found_words = dict_word[0]
+                    else:
+                        found_words = found_words + ", " + dict_word[0]
         elif language == '-ar':
             for dict_word in self.wordlist:
                 if word.lower() in dict_word[0].lower():
-                    found_words = found_words + ", " + dict_word[1]
+                    if found_words == "":
+                        found_words = dict_word[1]
+                    else:
+                        found_words = found_words + ", " + dict_word[1]
         if found_words == "":
             return "Unable to find word"
         else:
             return found_words
 
-
     def translate_direct(self, language, sentence):
-        #Do somthing
-        sentence = sentence.split()
-        translated_sentence = ""
+        # Do somthing
+        translated = []
+
         for word in sentence:
-            translated_sentence = translated_sentence + self.translate_single_word(language, word)
-        return translated_sentence
+            translated.append(self.translate_single_word(language, word))
+        print(translated)
+        return translated
--- text-translator.py
+++ text-translator.py
@@ -3,79 +3,88 @@ import os
 import lib.dictionary as d
 import lib.translator as t
 
+
 def main():
-##Main menu for the program
-	print("Text translator for English and Eastern Central Arrernte")
-	dictionary = d.Dictionary()
-	translator = t.Translator(dictionary)
-	menu(sys.argv, dictionary, translator)
+    # Main menu for the program
+    print("Text translator for English and Eastern Central Arrernte")
+    dictionary = d.Dictionary()
+    translator = t.Translator(dictionary)
+    menu(sys.argv, dictionary, translator)
+
 
 def menu(arguments, dictionary, translator):
 
-	#Options:
-	#No arguments given, print basic help (sys.argv only has python file name as arg[0])
-	if len(arguments) == 1:
-		print(text_basic_help)
-	elif arguments[1] == '-h' or arguments[1] == '--help':
-	#-h or --help
-		print(text_help)
-	elif arguments[1] == '-s' or arguments[1] == '--single-word':
-	#Single text using -s or --single-word
-		if len(arguments) == 2:
-			#If language flag not included
-			print("Please include source language as flags -en or -ar")
-		elif  arguments[2] == '-en' or arguments[2] == '-ar':
-			#Language flag included
-			if len(arguments) == 3:
-				#no word was provided
-				print("Please enter word to translate")
-			else:
-				if len(arguments) >= 5:
-					print("Multiple words detected, only translating the first word")
-				print(dictionary.lookup_word(arguments[2], arguments[3]))
-		else:
-			print("Please include source language as flags -en or -ar")
+    # Options:
+    # No arguments given, print basic help (sys.argv only has python file name
+    # as arg[0])
+    if len(arguments) == 1:
+        print(text_basic_help)
+    elif arguments[1] == '-h' or arguments[1] == '--help':
+        #-h or --help
+        print(text_help)
+    elif arguments[1] == '-s' or arguments[1] == '--single-word':
+        # Single text using -s or --single-word
+        if len(arguments) == 2:
+            # If language flag not included
+            print("Please include source language as flags -en or -ar")
+        elif arguments[2] == '-en' or arguments[2] == '-ar':
+            # Language flag included
+            if len(arguments) == 3:
+                # no word was provided
+                print("Please enter word to translate")
+            else:
+                if len(arguments) >= 5:
+                    print("Multiple words detected, only translating the first word")
+                print(
+                    translator.translate_single_word(
+                        arguments[2], arguments[3]))
+        else:
+            print("Please include source language as flags -en or -ar")
 
-	elif arguments[1] == '-d' or arguments[1] == '--direct':
-		print("Direct translation: ")
-		if len(arguments) == 2:
-			#If language flag not included
-			print("Please include source language as flags -en or -ar")
-		elif  arguments[2] == '-en' or arguments[2] == '-ar':
-			#Language flag included
-			if len(arguments) == 3:
-				#no word was provided
-				print("Please enter a sentence to translate")
-			else:
-				i = 3
-				to_translate = ""
-				while i < len(arguments):
-					to_translate = to_translate + " " + arguments[i]
-					i += 1
-				print(translator.translate_direct(arguments[2], to_translate))
-		else:
-			print("Please include source language as flags -en or -ar")
-	elif arguments[1] == '-ml':
-		print("Machine Learning Translation")
+    elif arguments[1] == '-d' or arguments[1] == '--direct':
+        print("Direct translation: ")
+        if len(arguments) == 2:
+            # If language flag not included
+            print("Please include source language as flags -en or -ar")
+        elif arguments[2] == '-en' or arguments[2] == '-ar':
+            # Language flag included
+            if len(arguments) == 3:
+                # no word was provided
+                print("Please enter a sentence to translate")
+            else:
+                i = 3
+                to_translate = ""
+                while i < len(arguments):
+                    to_translate = to_translate + " " + arguments[i]
+                    i += 1
+                for word in translator.translate_direct(
+                        arguments[2], to_translate):
+                    print(word)
+                print(translator.translate_direct(arguments[2], to_translate))
+        else:
+            print("Please include source language as flags -en or -ar")
+    elif arguments[1] == '-ml':
+        print("Machine Learning Translation")
 
-		if len(arguments) == 2:
-			#If language flag not included
-			print("Please include source language as flags -en or -ar")
-		elif  arguments[2] == '-en' or arguments[2] == '-ar':
-			#Language flag included
-			if len(arguments) == 3:
-				#no word was provided
-				print("Please enter a sentence to translate")
-			else:
-				print("Translating")
-		else:
-			print("Please include source language as flags -en or -ar")
-	else:
-		print("Unknown arguments given to the program")
-		print(text_basic_help)
+        if len(arguments) == 2:
+            # If language flag not included
+            print("Please include source language as flags -en or -ar")
+        elif arguments[2] == '-en' or arguments[2] == '-ar':
+            # Language flag included
+            if len(arguments) == 3:
+                # no word was provided
+                print("Please enter a sentence to translate")
+            else:
+                print("Translating")
+        else:
+            print("Please include source language as flags -en or -ar")
+    else:
+        print("Unknown arguments given to the program")
+        print(text_basic_help)
+
 
 text_basic_help = "Help: [-h/--help]\nSingle word: [-s/--single-word] [-en/-ar] <word>.\nDirect translation [-d/--direct] [-en/-ar] <Body of test>.\nMachine Learning Translating [-ml] [-en/-ar] <Body of text>\nExample: python3 main.py -d -ar Werte ahelhe!"
 
 text_help = "Help: [-h/--help]\nSingle Word Translating is achieved by a word lookup in the Data/ENG-CEA-Dictionary.txt file. It should be noted that the wordlist is a work in progress as there is no central digital dictionary to use and this is created from different resources. The CEA Acronym stands for Central-Eastern Arrernte\nUsage: [-s/--single-word] [-en/-ar] <word>.\nExample: python3 text-translator.py -s -en Hello\n\nDirect translation is similar to the single word translation. Does not take into account syntax.\nUsage [-d/--direct] [-en/-ar] <Body of test>.\nExample: python3 text-translator.py -d -ar Werte ahelhe!\n\nThe machine learning translation is a work in progress still. It is implemented using Google's Tensor Flow. Due to the limited dataset, it may prove innacurate.\nUsage: [-ml] [-en/-ar] <Body of text>\nExample: python3 text-translator.py -ml -en Hey! Over there!\n "
 if __name__ == "__main__":
-	main()
+    main()
